415cab7aba61fcfb9b4a3f2f81828599f28ee6d8
naeyanika <40598758+naeyanika@users.noreply.github.com>
2024-07-31 13:46:00 +0700

    Update tlp-kdp.py

--- tlp-kdp.py
+++ tlp-kdp.py
@@ -205,7 +205,8 @@ if uploaded_files:
 
         pivot_table4.columns = [f'{col[0]}_{col[1]}' for col in pivot_table4.columns]
         pivot_table4.reset_index(inplace=True)
-
+        
+         pivot_table2['TRANS. DATE'] = pd.to_datetime(pivot_table2['TRANS. DATE'], format='%d%m%Y').dt.strftime('%d/%m/%Y')
 
         # Add missing columns
         for col in new_columns:
